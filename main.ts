b040f30e9a3efbb0e87f5b5a716a8300d365f85c
Yaroslav <q.link0.p@gmail.com>
2019-03-24 13:41:24 +0200

    sync

--- main.ts
+++ main.ts
@@ -1,5 +1,11 @@
 import { BrowserWindow, app } from 'electron';
 import { platform } from 'os';
+let fs = require('fs');
+
+var preferencePath = process.env.APPDATA || (process.platform == 'darwin' ? process.env.HOME + 'Library/Preferences' : process.env.HOME + "/.local/share")
+preferencePath += "/remote-music-preference.json"
+
+let requireSetup = fs.existsSync(preferencePath)
 
 let win: BrowserWindow = null
 
@@ -8,17 +14,22 @@ function createWindow(){
         width: 600,
         height: 400,
         frame: true
-    });
+    })
 
     win.loadFile("src/app/index.html");
 }
 
-app.on('ready', createWindow);
+app.on('ready', (launchParams) => {
+    if (requireSetup) {
+        //Show setup window first
+    }
+    createWindow();
+});
 
 app.on('window-all-closed', () =>{
     if (platform() != "darwin"){
         app.quit();
     }
-});
+})
 
 app.on('activate', createWindow)
---
+++ src/core/preferences.ts
@@ -0,0 +1,16 @@
+let fs = require('fs');
+
+export class Preferences {
+    server, 
+}
+
+export class PreferenceLoader {
+    
+    readPref : function (path: String) -> Preferences {
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+}
